6145fc0f3fcce96060c826959f42466c0cb90703
DanielleHefer <daniellehefer1@gmail.com>
2021-12-02 19:26:22 +0200

    Initial commit

---
+++ src/MessageBusImpl.java
@@ -0,0 +1,69 @@
+package bgu.spl.mics;
+
+/**
+ * The {@link MessageBusImpl class is the implementation of the MessageBus interface.
+ * Write your implementation here!
+ * Only private fields and methods can be added to this class.
+ */
+public class MessageBusImpl implements MessageBus {
+
+	@Override
+	public <T> void subscribeEvent(Class<? extends Event<T>> type, MicroService m) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void subscribeBroadcast(Class<? extends Broadcast> type, MicroService m) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public <T> void complete(Event<T> e, T result) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void sendBroadcast(Broadcast b) {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+	@Override
+	public <T> Future<T> sendEvent(Event<T> e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void register(MicroService m) {
+		// TODO Auto-generated method stub
+		// "it creates a queue for each micro service using the register method" *******
+
+	}
+
+	@Override
+	public void unregister(MicroService m) {
+		// TODO Auto-generated method stub
+		// "when a micro service calls the unregister method of the message bus, the message bus should remove itsqueue *******
+		// and clean all references related to that micro service" *******
+
+
+	}
+
+	@Override
+	public Message awaitMessage(MicroService m) throws InterruptedException {
+		// TODO Auto-generated method stub
+
+		// " Once the queue is created, a Micro-Service can take the next message in the queue using the ‘awaitMessage’ *******
+		//method. The ‘awaitMessage’ method is blocking, that is, if there are no messages *******
+		//available in the Micro-Service queue, it should wait until a message becomes available." *******
+		return null;
+	}
+
+	
+
+}
